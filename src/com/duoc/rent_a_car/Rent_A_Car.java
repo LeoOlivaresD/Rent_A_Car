0e6165e450345005993e2fd2c29d58f3f5a14e1a
LeoOlivaresD <cspraword@gmail.com>
2024-07-10 00:01:48 -0400

    Se confecciona logica del menu

---
+++ src/com/duoc/rent_a_car/Rent_A_Car.java
@@ -0,0 +1,63 @@
+package com.duoc.rent_a_car;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Rent_A_Car {
+
+    static Scanner sc = new Scanner(System.in);
+    static int opcionMenu = 0;
+    static boolean romperBucle = false;
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al sistema de renta de autos Brief Drive");
+        menu();
+    }
+
+    public static void menu() {
+        do {
+            try {
+                System.out.println("Elige alguna de las siguientes opciones, escribiendo el numero segun corresponda");
+                System.out.println("1: Registrar usuario (selecciona esta opcion para poder arrendar vehiculos.)\n"
+                        + "2: Mostrar tarifas de arriendo.\n"
+                        + "3: Arrendar vehiculo.\n"
+                        + "4: Ver listado de vehiculos arrendados.\n"
+                        + "5: Ver vehiculos arrendados por mas de una semana.\n"
+                        + "6: Sair del sistema.");
+                opcionMenu = sc.nextInt();
+            } catch (InputMismatchException e) {
+                e.printStackTrace();
+                System.out.println("ERROR, debe ingresar un NUMERO para validar la opcion ingresada");
+                sc.next(); //limpio el buffer
+            }
+            switch (opcionMenu) {
+                //REGISTRO USUARIO
+                case 1:
+
+                    break;
+                // MOSTRAR TARIFAS DE ARRIENDO
+                case 2:
+                    break;
+                //ARRENDAR VEHICULO
+                case 3:
+                    break;
+                //VER LISTADO DE VEHICULOS ARRENDADOS
+                case 4:
+                    break;
+                //VER ARRIENDOS DE MAS DE UNA SEMANA
+                case 5:
+                    break;
+                //SALIR
+                case 6:
+                    System.out.println("Saliendo del sistema, gracias por preferir\n"
+                            + "Rent a Car Brief Drive.");
+                    romperBucle = true;
+                    break;
+                //ELECCION INCORRECTA
+                default:
+                    System.out.println("Lo sentimos, la opcion ingresada no existe");
+            }
+        } while (!romperBucle);
+        sc.close();//Se cierra el objeto
+    }
+}
