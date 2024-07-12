ec6d5ab050950acc457a7f099339d2be40b5a5e6
LeoOlivaresD <cspraword@gmail.com>
2024-07-12 00:39:48 -0400

    Se crea esta clase para operar con constantes y definir descuentos, aplicar IVA y mostrar boletas

---
+++ src/com/duoc/rent_a_car/entites/client/Finanzas.java
@@ -0,0 +1,8 @@
+package com.duoc.rent_a_car.entites.client;
+//EN ESTA CLASE ALMACENARE MIS CONSTANSTES PARA REALIZAR CALCULOS
+
+public class Finanzas {
+
+    private final Double IVA = 0.19;
+    private final Double DESCUENTO_CLIENTE_NUEVO = 0.10;
+}
