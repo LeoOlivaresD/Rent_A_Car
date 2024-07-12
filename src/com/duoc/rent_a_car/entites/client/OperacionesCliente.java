5d8b3bee74be74ade3c2ae396f691d87410057d7
LeoOlivaresD <cspraword@gmail.com>
2024-07-12 00:38:27 -0400

    Se crea clase para operar con clientes. Esta clase me permite registrar clientes, mostrar lista de clientes y buscar clientes por id. A futuro se implememtaran mas metodos segun necesidad

--- src/com/duoc/rent_a_car/entites/client/OperacionesCliente.java
+++ src/com/duoc/rent_a_car/entites/client/OperacionesCliente.java
@@ -1,5 +1,105 @@
 package com.duoc.rent_a_car.entites.client;
 
-public class OperacionesCliente {
-    
+import com.duoc.rent_a_car.interfaces.IOperacionesCliente;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class OperacionesCliente implements IOperacionesCliente {
+    private static final Scanner sc = new Scanner(System.in);
+    private Cliente cliente;
+    private List<Cliente> listaClientes;
+
+    public OperacionesCliente() {
+        this.listaClientes = new ArrayList<>();
+    }
+
+    public OperacionesCliente(Cliente cliente, List<Cliente> listaClientes) {
+        this.cliente = cliente;
+        this.listaClientes = listaClientes;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public List<Cliente> mostrarClientes() {
+        System.out.println("Mostrando id de clientes");
+        for (Cliente c : listaClientes ) {
+            System.out.println("Id Cliente: " + c.getIdCliente() + " Nombre cliente: " + c.getNombreCliente()); 
+        }
+        return listaClientes;
+    }
+
+    //Metodo implementdo por interface
+    //CON CADA LLAMADA AL METODO CREARÉ NUEVOS USUARIOS QUE ALACENARÉ EN MI LISTA
+    @Override
+    public void registrarUsuario(Cliente cliente) {
+        Boolean cicloDoWhile = true;
+        Random rand = new Random();
+        int min = 1;
+        int max = 10000;
+        int idAleatorio = rand.nextInt(max - min + 1) + min;
+        cliente.setIdCliente(idAleatorio);
+        System.out.println("Id creado para usuario: " + cliente.getIdCliente());
+        do {
+            System.out.println("Ingrese nombre de usuario a registrar");
+            String nombre = sc.nextLine();
+            if (nombre.isBlank() || nombre.isEmpty()) {
+                System.out.println("Debe ingresar un nombre valido, por favor vuelva a intentarlo");
+            } else {
+                cliente.setNombreCliente(nombre);
+                System.out.println("Nombre registrado !");
+                cicloDoWhile = false;
+            }
+        } while (cicloDoWhile);
+        cicloDoWhile = true; //Reseteo mi variable para que continue de manera correcta el flujo del programa
+
+        do {
+            try {
+                System.out.println("Ingrese telefono de usuario a registrar (9 digitos)");
+                int telefono = sc.nextInt();
+
+                String telefonoACadena = String.valueOf(telefono);
+                if (telefonoACadena.isBlank() || telefonoACadena.isEmpty()) {
+                    System.out.println("Debe ingresar un numero valido, por favor vuelva a intentarlo");
+                } else {
+                    cliente.setTelefono(telefono);
+                    System.out.println("Telefono registrado !");
+                    //ALMACENO MI USUARIO EN LA LISTA
+                    listaClientes.add(cliente);
+                    System.out.println("USUARIO REGISTRADO !");
+                    cicloDoWhile = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("ERROR, debe ingresar solo numeros");
+                e.printStackTrace();
+                sc.next(); //LIMPIO BUFFER
+            }
+        } while (cicloDoWhile);
+        
+    }
+
+    @Override
+    public Cliente obtenerClientePorId(int idCliente) {
+        for(Cliente cliente : listaClientes){
+        if(cliente.getIdCliente() == idCliente)
+           return cliente;
+        }
+        return null;
+    }
 }
