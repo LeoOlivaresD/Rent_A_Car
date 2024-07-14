4088c842d119dc986909bd7196bd0600176489c8
LeoOlivaresD <cspraword@gmail.com>
2024-07-13 23:28:51 -0400

    Se generan metodos para aplicar, iva, descuentos, calcular subTotal

--- src/com/duoc/rent_a_car/entites/client/Finanzas.java
+++ src/com/duoc/rent_a_car/entites/client/Finanzas.java
@@ -1,8 +1,35 @@
 package com.duoc.rent_a_car.entites.client;
 //EN ESTA CLASE ALMACENARE MIS CONSTANSTES PARA REALIZAR CALCULOS
 
+import com.duoc.rent_a_car.entites.cars.Vehiculo;
+import java.util.Map.Entry;
+
 public class Finanzas {
 
     private final Double IVA = 0.19;
     private final Double DESCUENTO_CLIENTE_NUEVO = 0.10;
+
+    //METODO PARA APLICAR IVA SOBRE EL VALOR BASE DEL ARRIENDO DE UN AUTO
+    public int aplicarIVA(int valorArriendo) {
+        int valorConIva = (int) ((valorArriendo * IVA) + valorArriendo);
+        return valorConIva;
+    }
+
+    //METODO QUE APLICARA DESCUENTOS A CLIENTES NUEVOS
+    public int aplicarDescuentoClienteNuevo(int valorArriendo) {
+        int valorConDescuento = (int) (valorArriendo - (valorArriendo * DESCUENTO_CLIENTE_NUEVO));
+        return valorConDescuento;
+    }
+    
+    public int calcularTotalServicio(int idCliente, OperacionesCliente operacionesCliente) {
+        int valorTotalServicio = 0;
+        //POR MEDIO DEL ID DETECTO EL CLIENTE ESPECIFICO DEL CUAL SE CALCULARA EL TOTAL DE SUS SERVICIOS 
+        Cliente clienteEncontrado = operacionesCliente.obtenerClientePorId(idCliente);
+        for(Entry<String, Vehiculo> vehiculoDetectado : clienteEncontrado.getListaVehiculos().entrySet()){
+        valorTotalServicio += vehiculoDetectado.getValue().getValorArriendo();
+        }
+        
+        return valorTotalServicio ;
+    }
+
 }
