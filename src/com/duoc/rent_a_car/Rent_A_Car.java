ef483ad308a8e9dea037d39dac4fcb800e44f01e
LeoOlivaresD <cspraword@gmail.com>
2024-07-16 12:26:12 -0400

    refactor

--- src/com/duoc/rent_a_car/Rent_A_Car.java
+++ src/com/duoc/rent_a_car/Rent_A_Car.java
@@ -12,6 +12,15 @@ import com.duoc.rent_a_car.outputs.VehiclePersistentOuput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/*
+Buenas profesor aca un pequeño resumen del proyecto para que no busque todas las cosas con mucho esfuerzo.
+Partiendo con el uso de hilos, decidi usarlos para manejar la carga de informacion desde los archivos txt
+tanto de clientes como autos registrados.
+En cuanto a las listas de autos, use HashMap y ademas utilice listas syncronizadas para actualizar toda la info.
+De hecho una vez que carga la informacion desde el archivo txt todo se agrega a la lista sincronizada de vehiculos y clientes.
+En cuanto  la generacion de boleta y arriendos de vehiculos, quise hacerlo apto para cualquier usuario sin necesidad de que deba logearse
+para ello cada vez que se intentan realizar estas acciones, se solicita el id del usuario en cuestion.
+ */
 public class Rent_A_Car {
 
     static ClientPersistentInput clientPersistentInput = new ClientPersistentInput();
@@ -26,12 +35,12 @@ public class Rent_A_Car {
     static boolean romperBucle = false;
 
     public static void main(String[] args) {
-        //COMIENZO A INSTANCIAS MIS CLASES QUE MANEJRAN HILOS PARA CARGAR LA INFORMACION DE CLIENTES Y VEHICULOS
+        //COMIENZO A INSTANCIAR MIS CLASES QUE MANEJARAN HILOS PARA CARGAR LA INFORMACION DE CLIENTES Y VEHICULOS
         Thread clienteThread = new Thread(new CargarDataCliente(operacionesCliente, clientPersistentInput));
         Thread vehiculoThread = new Thread(new CargarDataVehiculo(persistentInput, operacionesVehiculo));
-        
+
         //INICIO LOS HILOS
-        clienteThread.start(); 
+        clienteThread.start();
         vehiculoThread.start();
 
         try {
@@ -43,7 +52,7 @@ public class Rent_A_Car {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        
+
         menu();
     }
 
@@ -57,9 +66,10 @@ public class Rent_A_Car {
                 System.out.println("| 1: Registrar usuario (selecciona esta opcion para poder arrendar vehiculos.)                     |");
                 System.out.println("| 2: Mostrar tarifas de arriendo.                                                                  |");
                 System.out.println("| 3: Arrendar vehiculo.                                                                            |");
-                System.out.println("| 4: Ver listado de vehiculos arrendados.                                                          |");
+                System.out.println("| 4: Ver listado de vehiculos disponibles.                                                          |");
                 System.out.println("| 5: Ver vehiculos arrendados por mas de una semana.                                               |");
-                System.out.println("| 6: Sair del sistema.                                                                             |");
+                System.out.println("| 6: Generar boleta.                                                                             |");
+                System.out.println("| 7: Salir del sistema.                                                                          |");
                 System.out.println("====================================================================================================");
 
                 opcionMenu = sc.nextInt();
@@ -73,12 +83,14 @@ public class Rent_A_Car {
                 case 1:
                     operacionesCliente.registrarUsuario(clientPersistent);
                     break;
-
                 //MOSTRAR TARIFAS DE ARRIENDO
                 case 2:
-
+                    System.out.println("Las siguientes tarifas estan excentas de IVA\n"
+                            + "Auto sedan: $50.000 por dia de arriendo.\n"
+                            + "Furgon: $60.000 por dia.\n"
+                            + "Camion: $120.000.\n"
+                            + "Si eres cliente nuevo contaras con un descuento del 10% sobre el valor total de la compra(se excluye el IVA)");
                     break;
-
                 //ARRENDAR VEHICULO
                 case 3:
                     operacionesCliente.mostrarClientes(); //muestro los clientes registradoss
@@ -86,23 +98,24 @@ public class Rent_A_Car {
                     int idClienteSeleccionado = sc.nextInt();
                     operacionesVehiculo.arrendarVehiculo(idClienteSeleccionado, operacionesCliente, ouput);
                     break;
-
-                //VER LISTADO DE VEHICULOS ARRENDADOS
+                //VER LISTADO DE VEHICULOS
                 case 4:
+                    System.out.println("Mostrando lista de todos los vehiculos");
                     operacionesVehiculo.listarTodosVehiculos();
                     break;
                 //VER ARRIENDOS DE MAS DE UNA SEMANA
                 case 5:
-                    String patente;
-                    System.out.println("Buscando un auto por patente, ingrese patente");
-                    patente = sc.nextLine();
-                    operacionesVehiculo.buscarVehiculoPorPatente(patente);
+                    System.out.println("Mostrando vehiculos arrendados por mas de una semana");
+                    operacionesVehiculo.filtrarArriendosPorMasDeUnaSemana();
                     break;
-                //SALIR
+                //GENERAR BOLETA Y SALIR
                 case 6:
+                    operacionesCliente.mostrarClientes(); //muestro los clientes registradoss
                     System.out.println("Ingrese el id del cliente deseaado para generar boleta");
                     int idCliente = sc.nextInt();
                     operacionesCliente.generarBoleta(idCliente, finanzas, operacionesVehiculo);
+                    break;
+                case 7:
                     System.out.println("Saliendo del sistema, gracias por preferir\n"
                             + "Rent a Car Brief Drive.");
                     romperBucle = true;
